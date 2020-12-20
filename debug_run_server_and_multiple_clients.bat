12675a11e36d185922dfdf88df7878bd01d207d6
Mathias Ericsson <mathias.ericsson@gmail.com>
2020-12-20 16:53:47 +0100

    Fixed starting server and multiple client scripts

---
+++ debug_run_server_and_multiple_clients.bat
@@ -0,0 +1,9 @@
+START %~dp0bin\Debug\AsteroidsServer.exe
+
+
+START %~dp0bin\Debug\AsteroidsClient.exe
+START %~dp0bin\Debug\AsteroidsClient.exe
+START %~dp0bin\Debug\AsteroidsClient.exe
+START %~dp0bin\Debug\AsteroidsClient.exe
+START %~dp0bin\Debug\AsteroidsClient.exe
+
---
+++ run_server_and_multiple_clients.bat
@@ -0,0 +1,8 @@
+START %~dp0bin\Release\AsteroidsServer.exe
+
+
+START %~dp0bin\Release\AsteroidsClient.exe
+START %~dp0bin\Release\AsteroidsClient.exe
+START %~dp0bin\Release\AsteroidsClient.exe
+START %~dp0bin\Release\AsteroidsClient.exe
+START %~dp0bin\Release\AsteroidsClient.exe
